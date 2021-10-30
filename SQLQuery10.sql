2479bfa40f523da569b031d5657ec46b819bcea2
ambbd <tehie.amb3.x@gmail.com>
2021-10-30 17:01:47 -0400

    deleted unecessary files

--- SQLQuery10.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT(Class),Color, COUNT(Class) AS 'Class Total',[ProductNumber]
-FROM [Production].[Product]
-WHERE Color IS NOT NULL AND Class IS NOT NULL
-GROUP BY Class, Color
---HAVING COUNT(Class) > 10
---ORDER BY Class DESC, COUNT(Class) DESC
--- SQLQuery13.sql
+++
@@ -1,8 +0,0 @@
---SELECT DISTINCT(Class),Color, COUNT(Class) AS 'Class Total'
---FROM [Production].[Product]
---WHERE Color IS NULL --AND Class IS NULL
---GROUP BY Class, Color
---ORDER BY Class DESC, COUNT(Class) DESC
-
-SELECT DISTINCT(SellStartDate)
-FROM Production.Product
